93504946160be8045532261bfff9a7fc77c64b38
marcapedoh <adralabel@gmail.com>
2024-11-20 08:02:00 +0000

    structure without signal

---
+++ src/app/headaer/headaer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headaer',
+  standalone: true,
+  imports: [],
+  templateUrl: './headaer.component.html',
+  styleUrl: './headaer.component.css'
+})
+export class HeadaerComponent {
+
+}
