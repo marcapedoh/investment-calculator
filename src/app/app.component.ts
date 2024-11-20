ef38581fb1d071d8ce6c044767f9bbd4daebdbb8
marcapedoh <adralabel@gmail.com>
2024-11-20 08:32:27 +0000

    upgrade an application using service and component injection instead of component decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { HeadaerComponent } from './headaer/headaer.component';
 import { UserInputComponent } from './user-input/user-input.component';
-import { type InvestmentInput } from './investment-input.model';
 import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
@@ -11,36 +10,7 @@ import { InvestmentResultsComponent } from './investment-results/investment-resu
   imports: [HeadaerComponent, UserInputComponent, InvestmentResultsComponent]
 })
 export class AppComponent {
-  resultsData = signal<{
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number,
-  }[] | undefined>(undefined);
-  calculateInvestmentResults(data: InvestmentInput) {
-    const annualData = [];
-    const { initialInvestment, duration, expectedReturn, annualInvestment } = data;
-    let investmentValue = initialInvestment;
 
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
 
-    this.resultsData.set(annualData);
-  }
 
 }
